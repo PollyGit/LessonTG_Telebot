08274a8d6ecf0ca595a943c6a3a3d029e620fcb7
PollyGit <63475486+PollyGit@users.noreply.github.com>
2025-01-25 21:59:45 +0300

    TG05_hw_make tg bot _TradeTicker

---
+++ TG05_hw_tg_bot_API/config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+TOKEN2 = os.getenv('TOKEN2')
+THE_CAT_API_KEY = os.getenv('THE_CAT_API_KEY')
+NASA_API_KEY = os.getenv('NASA_API_KEY')
+POLYGON_API_KEY = os.getenv('POLYGON_API_KEY')
+TickerTradeInfo_bot = os.getenv('TickerTradeInfo_bot')
